67b7bce4c5a5e3535a6c3ee354ba49b92cad6b23
CrazyJ36 <crazy_j36@hotmail.com>
2025-05-08 00:33:47 -0400

    u

--- reaper-reascript/Nektar Pacer LEDs.lua
+++ reaper-reascript/Nektar Pacer LEDs.lua
@@ -3,41 +3,59 @@
 deviceMode = 23 -- 16 + reaper midi output device id.
 msg_type = 11 -- CCs are message type 11, Note on: 8, etc.
 channel = 0 -- actual channel - 1.
+cc = 0
 
+gotLastTrackValue = {}
 
 function run()
-  retval, buf, ts, devIdx, projPos, projLoopCnt = reaper.MIDI_GetRecentInputEvent(0)
-  receivedCC = string.byte(buf, 2)
-  receivedValue = string.byte(buf, 3)
+  inputEvent = {reaper.MIDI_GetRecentInputEvent(0)} -- table of returned values.
+  cc = string.byte(inputEvent[2], 2)
+  ccValue = string.byte(inputEvent[2], 3)
   
-  track = reaper.GetSelectedTrack(0, 0)
-  last_retva, last_track_id, last_fx_id, last_fx_param = reaper.GetLastTouchedFX()
-  
-  if track then
-    val, track, fxid, paramid, minval, maxval = reaper.TrackFX_GetParam(track, last_fx_id, last_fx_param)
-    reaper.ShowConsoleMsg(val .. "\n")
-  end
-  
-  if receivedCC ~= lastStoredCC then
-    if receivedValue == 0 then
-      reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, 0, 0)
+  if cc ~= lastCC or ccValue ~= lastCCValue then
+    if ccValue == 0 then
+      reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, cc, 0)
     else 
-      reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, 0, 127)
+      reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, cc, 127)
     end
-    reaper.ShowConsoleMsg(receivedCC .. " " .. receivedValue .. "\n")
-    lastStoredCC = receivedCC
+    reaper.ShowConsoleMsg(cc .. " " .. ccValue .. "\n\n")
+    lastCC = cc
+    lastCCValue = ccValue
   end
+  
+  num_tracks = reaper.CountSelectedTracks(0)
+  for i = 0, num_tracks - 1 do
+    gotLastTrackValue[i] = false
     
+    if not gotLastTrackValue[i] then
+    
+      track = reaper.GetSelectedTrack(0, i)
+      if track then
+        lastTouchedFx = {reaper.GetLastTouchedFX()}
+        last_fx_id = lastTouchedFx[3]
+        last_fx_param = lastTouchedFx[4]
+    
+        paramValue = {reaper.TrackFX_GetParam(track, last_fx_id, last_fx_param)}
+        if paramValue[1] ~= lastParamValue then
+          reaper.ShowConsoleMsg(paramValue[1].."\n")
+          lastParamValue = paramValue[1]
+        end
+      end
+      gotLastTrackValue[i] = true
+    end
+  end
+ 
+  
+  
   reaper.defer(run)
 end
 
-
-function onexit()
-  reaper.ShowConsoleMsg("Exited");
+function onExit()
+  reaper.ShowConsoleMsg("\nExited");
 end
 
-reaper.defer(run)
-reaper.atexit(onexit)
+run()
+reaper.atexit(onExit)
 
  
 
