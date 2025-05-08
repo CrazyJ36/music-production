a71ca932ede24e63723c14de83a11180e6f48cd6
CrazyJ36 <crazy_j36@hotmail.com>
2025-05-08 02:36:21 -0400

    u

--- reaper-reascript/Nektar Pacer LEDs.lua
+++ reaper-reascript/Nektar Pacer LEDs.lua
@@ -4,19 +4,19 @@ deviceMode = 23 -- 16 + reaper midi output device id.
 msg_type = 11 -- CCs are message type 11, Note on: 8, etc.
 channel = 0 -- actual channel - 1.
 cc = 0
-trackValues = {}
+lastTrackParamValues = {}
 
 function run()
   inputEvent = {reaper.MIDI_GetRecentInputEvent(0)} -- table of returned values.
   cc = string.byte(inputEvent[2], 2)
   ccValue = string.byte(inputEvent[2], 3)
   if cc ~= lastCC or ccValue ~= lastCCValue then
-    if ccValue == 0 then
+    --[[if ccValue == 0 then
       reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, cc, 0)
     else 
       reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, cc, 127)
-    end
-    reaper.ShowConsoleMsg(cc .. "-" .. ccValue .. "\n\n")
+    end]]
+    reaper.ShowConsoleMsg("Input CC and Value: "..cc .. "-" .. ccValue .. "\n\n")
     lastCC = cc
     lastCCValue = ccValue
   end
@@ -29,9 +29,14 @@ function run()
       track = reaper.GetSelectedTrack2(0, i, true)
       paramInfo = {reaper.TrackFX_GetParam(track, last_fx_id, last_fx_param)}
       paramValue = paramInfo[1]
-      if paramValue ~= trackValues[i] then
-        reaper.ShowConsoleMsg(paramValue.."\n")
-        trackValues[i] = paramValue
+      if paramValue ~= lastTrackParamValues[i] then
+        if paramValue == 0 then
+          reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, cc, 0)
+        else 
+          reaper.StuffMIDIMessage(deviceMode, msg_type * 16 + channel, cc, 127)
+        end
+        reaper.ShowConsoleMsg("Current Param Value: "..paramValue.."\n")
+        lastTrackParamValues[i] = paramValue
       end
   end
 
