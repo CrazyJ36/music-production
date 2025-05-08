90ff53357e31ef2576c50fdfdcd292085f9061fb
CrazyJ36 <crazy_j36@hotmail.com>
2025-05-07 21:05:53 -0400

    u

--- reaper-reascript/ReaScript Loop Example.lua
+++ reaper-reascript/ReaScript Loop Example.lua
@@ -1,19 +1,19 @@
 loopCount = 0;
 commandId = reaper.NamedCommandLookup("_RS0a2c9b10e87f8321f19ececd4fa699dab4a6b959");
 
-gfx.init("ReaScript Loop Example");
-
-char = gfx.getchar()
+gfx.init("ReaScript Loop Example LUA");
 
 function run()
-  if gfx.getchar() > 0 then
-    return
-  end
-
   loopCount = loopCount + 1;
   gfx.x = 1;
+  gfx.y = 1;
+  gfx.drawstr("Press \'q\' to exit...\n\n");
   gfx.printf("Loop Count:%d", loopCount);
-  reaper.defer(run);
+
+  if gfx.getchar() ~= string.byte('q') then
+    reaper.defer(run)
+  end
+  
 end
 
 function onExit()
@@ -21,5 +21,5 @@ function onExit()
   reaper.ShowConsoleMsg("Exited Successfully!")
 end
 
-reaper.defer(run)
-reaper.atexit(onExit);
+run()
+reaper.atexit(onExit)
