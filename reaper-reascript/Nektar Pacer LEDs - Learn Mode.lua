ba1637c9c7bdba847d78999d81f6374a748718a0
CrazyJ36 <crazy_j36@hotmail.com>
2025-05-11 20:22:58 -0400

    u

--- reaper-reascript/Nektar Pacer LEDs - Learn Mode.lua
+++ reaper-reascript/Nektar Pacer LEDs - Learn Mode.lua
@@ -8,7 +8,7 @@
 --msgType = inputEvent[2]:byte(1) & 0xF0
 
 deviceMode = 23 -- 16 + reaper midi output device id.
-ccCount = 1 -- 1 for two pedals
+ccCount = 2 -- 2 for two pedals
 
 learnBtnBounds = {5, 5, 60, 20}
 quitBtnBounds = {235, 5, 60, 20}
@@ -17,143 +17,154 @@ terminate = false
 learningState = false
 learningStateText = ""
 gotCC = false
-gotParam = false
-trackIter = 0
-selectedTrackOnly = false
 ccs = {}
 fx = {}
 params = {}
 minValues = {}
 maxValues = {}
-learnInc = 0
-lastInputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
+oldInputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
+inputEvent = nil
 lastTouchedFx = {reaper.GetLastTouchedFX()}
-lastParam = {
+lastParamId = {
   reaper.TrackFX_GetParamIdent(
     reaper.GetLastTouchedTrack(), 
     lastTouchedFx[3],
     lastTouchedFx[4]
   )
 }
+lastParam = {reaper.TrackFX_GetParam(
+  reaper.GetLastTouchedTrack(),
+  lastTouchedFx[3],
+  lastTouchedFx[4]
+)}
 reaper.ClearConsole()
 
 function main()
   gfx.init("Learn CCs then Params", 300, 100)
   ui()
-  if learningState then
+  if learningState and #ccs < ccCount then
     if not gotCC then
       learningStateText = "Learning CC"
       reaper.runloop(getCC())
-    elseif not gotParam then
+    else
       learningStateText = "Learning Param"
       reaper.runloop(getParam())
     end
-  else
-    reaper.runloop(setParam())
+  elseif #params > 0 then
+    setParam()
+    setLeds()
   end
+
   if terminate then 
     return
   end
   reaper.defer(main)
 end
 
-function getCC() 
-  reaper.ShowConsoleMsg("getCC()\n")
-  if reaper.MIDI_GetRecentInputEvent(0) ~= 0 then
-    currentInputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
-    if currentInputEvent[2]:byte(1) & 0xF0 == 176 and
-      currentInputEvent[2] ~= lastInputEvent[2] then
-      ccs[learnInc] = currentInputEvent[2]:byte(2)
-      lastInputEvent[2] = currentInputEvent[2]
-      gotCC = true
-      reaper.ShowConsoleMsg("returning from getCC\n")
-      return
-    end
+function getInputEvent() 
+  currentInputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
+  if currentInputEvent[1] ~= 0 and
+  currentInputEvent[2] ~= oldInputEvent[2] and
+  currentInputEvent[2]:byte(1) & 0xF0 == 176 then
+    oldInputEvent[2] = currentInputEvent[2]
+    cc = currentInputEvent[2]:byte(2)
+    ccValue = currentInputEvent[2]:byte(3)
+    channel = currentInputEvent[2]:byte(1) & 0x0F
+    msgType = currentInputEvent[2]:byte(1) & 0xF0
+    return {true, cc, ccValue, channel, msgType}
+  else 
+    return {false}
   end
 end
 
+function getCC()
+  inputEvent = getInputEvent()
+  if inputEvent[1] then
+    table.insert(ccs, inputEvent[2])
+    gotCC = true
+    reaper.ShowConsoleMsg("returning from getCC()\n")
+    return
+  end
+  inputEvent = nil
+end
+
 function getParam() 
-  reaper.ShowConsoleMsg("getParam()\n")
   lastTouchedFx = {reaper.GetLastTouchedFX()}
   currentParamId = {reaper.TrackFX_GetParamIdent(
     reaper.GetLastTouchedTrack(), 
     lastTouchedFx[3], lastTouchedFx[4]
   )}
-   
-  if currentParamId[1] and currentParamId[2] ~= lastParam[2] then
-    lastParam[2] = currentParamId[2]
-    fx[learnInc] = lastTouchedFx[3]
-    params[learnInc] = lastTouchedFx[4]
-    
-    param = {reaper.TrackFX_GetParam(
-      reaper.GetLastTouchedTrack(),
-      fx[learnInc],
-      params[learnInc]
-    )}
-    reaper.ShowConsoleMsg("fx: "..fx[learnInc].."\n"..params[learnInc].."\n")
-    minValues[learnInc] = param[2]
-    maxValues[learnInc] = param[3]
-    
-    learnInc = learnInc + 1
-    gotParam = true
-    if learningState then
-      gotCC =  false
-      gotParam = false
-      getCC()
-    end
+  currentParam = {reaper.TrackFX_GetParam(
+    reaper.GetLastTouchedTrack(),
+    lastTouchedFx[3],
+    lastTouchedFx[4]
+  )}
+  if currentParamId[1] and (currentParamId[2] ~= lastParamId[2] or 
+  currentParam[1] ~= lastParam[1]) then
+    lastParamId[2] = currentParamId[2]
+    lastParam[1] = currentParam[1]
+    table.insert(fx, lastTouchedFx[3])
+    table.insert(params, lastTouchedFx[4])
+    table.insert(minValues, currentParam[2])
+    table.insert(maxValues, currentParam[3])
+    gotCC =  false
     reaper.ShowConsoleMsg("returning from getParam\n")
     return
   end
 end
 
 function setParam()
-  reaper.ShowConsoleMsg("setParam()\n")
-  if selectedTrackOnly then
-    track = reaper.GetLastTouchedTrack()
-  else 
-    if trackIter == reaper.CountTracks(0) then
-      trackIter = 0
-    end
-    track = reaper.GetTrack(0, trackIter)
-  end
-  currentInputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
-  msg = currentInputEvent[2]
-  if msg ~= nil and currentInputEvent[1] ~= 0 then
-    channel = msg:byte(1) & 0x0F
-    msgType = msg:byte(1) & 0xF0
-    --if msg ~= lastInputEvent[2] and 
-    if msgType == 176 then
-      for i = 0, #ccs do
-      if msg:byte(2) == ccs[i] then
-        inputValue = msg:byte(3)
-        if inputValue == 127 then
-          reaper.ShowConsoleMsg("setting high\n")
+  inputEvent = getInputEvent()
+  if inputEvent[1] then
+    reaper.ShowConsoleMsg("setParam: getInputEvent[1] successful.\n")
+    cc = inputEvent[2]
+    ccValue = inputEvent[3]
+    for i = 1, #ccs do
+      if cc == ccs[i] then
+        if ccValue == 0 then
           reaper.TrackFX_SetParam(
-            track, 
+            reaper.GetLastTouchedTrack(),
+            fx[i],
+            params[i], 
+            minValues[i]
+          )
+  
+        else 
+          reaper.TrackFX_SetParam(
+            reaper.GetLastTouchedTrack(), 
             fx[i], 
             params[i], 
             maxValues[i]
           )
-          reaper.StuffMIDIMessage(deviceMode, msgType + channel, ccs[i], 127)
-        else
-          reaper.ShowConsoleMsg("setting low\n")
-           reaper.TrackFX_SetParam(
-             track,
-             fx[i],
-             params[i], 
-             minValues[i]
-          )
-          reaper.StuffMIDIMessage(deviceMode, msgType + channel, ccs[i], 0)
-         end
        end
      end
    end
-  end
-  if not selectedTrackOnly then 
-    trackIter = trackIter + 1 
-  end
+ end
 end
 
+function setLeds()
+  if inputEvent[1] then
+    reaper.ShowConsoleMsg("setLeds: getInputEvent[1] successful.\n")
+    for i = 1, #params do
+      channel = inputEvent[4]
+      msgType = inputEvent[5]
+      paramInfo = {reaper.TrackFX_GetParam(
+        reaper.GetLastTouchedTrack(), fx[i], params[i]
+      )}
+      if paramInfo[1] == 0 then
+        reaper.ShowConsoleMsg("Setting led low")
+        reaper.StuffMIDIMessage(
+          deviceMode, msgType + channel, ccs[i], 0
+        )
+      else 
+        reaper.ShowConsoleMsg("Setting led high")
+        reaper.StuffMIDIMessage(
+          deviceMode, msgType + channel, ccs[i], 127
+        )
+      end
+    end
+  end
+end
 
 function ui()
   gfx.rect(
@@ -179,7 +190,6 @@ function ui()
   gfx.mouse_x < learnBtnBounds[3] + learnBtnBounds[1] and
   gfx.mouse_y < learnBtnBounds[4] + learnBtnBounds[2] then
     gotCC = false
-    gotParam = false
     learningState = not learningState
   end
  
