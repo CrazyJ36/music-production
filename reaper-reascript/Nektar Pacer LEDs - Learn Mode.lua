8813cb9f54022effd2b6875fcbfa6da81f15ab39
CrazyJ36 <crazy_j36@hotmail.com>
2025-05-13 05:32:59 -0400

    u

--- reaper-reascript/Nektar Pacer LEDs - Learn Mode.lua
+++ reaper-reascript/Nektar Pacer LEDs - Learn Mode.lua
@@ -6,22 +6,63 @@
 --cc = msg:byte(2)
 --ccValue = msg:byte(3)
 --msgType = inputEvent[2]:byte(1) & 0xF0
+reaper.ClearConsole()
 
+scriptName = "Nektar Pacer LEDs - Learn Mode"
 deviceMode = 23 -- 16 + reaper midi output device id.
-maxLearnCount = 2 -- 2 for two pedals, starts at one.
+maxLearnCount = 4 -- starts at one.
 learnBtnBounds = {5, 5, 60, 20}
 quitBtnBounds = {335, 5, 60, 20}
-terminate = false
-learningState = false
-learningStateText = "Press Learn"
-mappedCCsText = ""
-gotCC = false
+resetBtnBounds = {335, 30, 60, 20}
+learningStateText = ""
+tracks = {}
 ccs = {}
 fxs = {}
-fxNames = {}
 params = {}
 minValues = {}
 maxValues = {}
+fxNames = {}
+if reaper.HasExtState(scriptName, "tracks") and 
+  reaper.HasExtState(scriptName, "ccs") and
+  reaper.HasExtState(scriptName, "fxs") and
+  reaper.HasExtState(scriptName, "params") and
+  reaper.HasExtState(scriptName, "minValues") and
+  reaper.HasExtState(scriptName, "maxValues") and
+  reaper.HasExtState(scriptName, "fxNames") then
+  storedTracks = reaper.GetExtState(scriptName, "tracks")
+  storedCCs = reaper.GetExtState(scriptName, "ccs")
+  storedFxs = reaper.GetExtState(scriptName, "fxs")
+  storedParams = reaper.GetExtState(scriptName, "params")
+  storedMinValues = reaper.GetExtState(scriptName, "minValues")
+  storedMaxValues = reaper.GetExtState(scriptName, "maxValues")
+  storedFxNames = reaper.GetExtState(scriptName, "fxNames")
+  for i in string.gmatch(storedTracks, '([^,]+)') do
+    table.insert(tracks, i)
+  end
+  for i in string.gmatch(storedCCs, '([^,]+)') do
+    table.insert(ccs, i)
+  end
+  for i in string.gmatch(storedFxs, '([^,]+)') do
+    table.insert(fxs, i)
+  end
+  for i in string.gmatch(storedParams, '([^,]+)') do
+    table.insert(params, i)
+  end
+  for i in string.gmatch(storedMinValues, '([^,]+)') do
+    table.insert(minValues, i)
+  end
+  for i in string.gmatch(storedMaxValues, '([^,]+)') do
+    table.insert(maxValues, i)
+  end
+  for i in string.gmatch(storedFxNames, '([^,]+)') do
+    table.insert(fxNames, i)
+  end
+  
+  for i = 1, #fxNames do
+    reaper.ShowConsoleMsg("tracks: "..tracks[i].."\n")
+  end
+end
+
 oldInputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
 oldTouchedFx = {reaper.GetLastTouchedFX()}
 oldParamId = {
@@ -36,39 +77,38 @@ oldParam = {reaper.TrackFX_GetParam(
   oldTouchedFx[3],
   oldTouchedFx[4]
 )}
-reaper.ClearConsole()
 
 function main()
   if terminate then 
     return
   end
-  
-  mappedCCsText = ""
-  for i = 1, #fxNames do
-    mappedCCsText = mappedCCsText.."CC "..
-      ccs[i]..": "..
-      fxNames[i].."\n"
-  end
-  if not learningState then 
-    learningStateText = "Press Learn"
-  end
-  
+
   if learningState then
-    --mappedCCsText = ""
-    if #params < maxLearnCount then
+    if #fxNames < maxLearnCount then
       if not gotCC then
         reaper.runloop(learnCC())
-      else
-        reaper.runloop(learnParam())
+      elseif not gotParam then
+        reaper.runloop(learnParam())  
       end
     else
       learningState = false
+      learningStateText = "Max CCs Learned"
     end
-  elseif #params > 0 then
+  elseif #fxNames > 0 then
     setParamAndLed()
   end
+
+  if not learningState then
+    learningStateText = "Press Learn"
+  end
+
+  mappedCCsText = ""
+  for i = 1, #fxNames do
+    mappedCCsText = mappedCCsText.."CC "..
+      ccs[i].." is mapped to track "..tracks[i]..":\n"..fxNames[i].."\n\n"
+  end
   
-  gfx.init("Learn CCs then Params", 400, 300)
+  gfx.init(scriptName, 400, 300)
   ui()
   reaper.defer(main)
 
@@ -95,8 +135,11 @@ function learnCC()
   inputEvent = getInputEvent()
   if inputEvent[1] then
     table.insert(ccs, inputEvent[2])
-    gotCC = true
-    reaper.ShowConsoleMsg("Returning from learnCC()\n")
+    if learningState then
+      gotCC = true
+      gotParam = false
+    end
+    reaper.ShowConsoleMsg("returning from learnCC()\n")
     return
   end
 end
@@ -116,8 +159,8 @@ function learnParam()
   if currentTouchedFx[1] and currentParamId[1] and 
   (currentParamId[2] ~= oldParamId[2] or 
   currentParam[1] ~= oldParam[1]) then
-    
-    
+    oldParamId[2] = currentParamId[2]
+    oldParam[1] = currentParam[1]
     fxName = {reaper.TrackFX_GetFXName(
       reaper.GetLastTouchedTrack(), 
       currentTouchedFx[3]
@@ -127,16 +170,23 @@ function learnParam()
       currentTouchedFx[3],
       currentTouchedFx[4]
     )}
-    paramText = fxName[2]..": "..paramName[2]
-    
-    oldParamId[2] = currentParamId[2]
-    oldParam[1] = currentParam[1]
+    table.insert(tracks, currentTouchedFx[2])
     table.insert(fxs, currentTouchedFx[3])
     table.insert(params, currentTouchedFx[4])
     table.insert(minValues, currentParam[2])
     table.insert(maxValues, currentParam[3])
-    table.insert(fxNames, paramText)
-    if learningState then gotCC =  false end
+    table.insert(fxNames, fxName[2]..": "..paramName[2])
+    reaper.SetExtState(scriptName, "tracks", table.concat(tracks, ","), false)
+    reaper.SetExtState(scriptName, "ccs", table.concat(ccs, ","), false)
+    reaper.SetExtState(scriptName, "fxs", table.concat(fxs, ","), false)
+    reaper.SetExtState(scriptName, "params", table.concat(params, ","), false)
+    reaper.SetExtState(scriptName, "minValues", table.concat(minValues, ","), false)
+    reaper.SetExtState(scriptName, "maxValues", table.concat(maxValues, ","), false)
+    reaper.SetExtState(scriptName, "fxNames", table.concat(fxNames, ","), false)
+    if learningState then
+      gotParam = true
+      gotCC = false
+    end
     reaper.ShowConsoleMsg("returning from learnParam()\n")
     return
   end
@@ -149,21 +199,22 @@ function setParamAndLed(track)
     ccValue = inputEvent[3]
     channel = inputEvent[4]
     msgType = inputEvent[5]
-    for i = 1, #params do
+    for i = 1, #fxNames do
       if cc == ccs[i] then
+        -- if fxName == bypass, switch min and max values.
         if ccValue == 0 then
           reaper.TrackFX_SetParam(
-            reaper.GetLastTouchedTrack(),
+            reaper.GetTrack(0, tracks[i] - 1),
             fxs[i] ,params[i],  minValues[i]
           )
         else 
           reaper.TrackFX_SetParam(
-            reaper.GetLastTouchedTrack(),
+            reaper.GetTrack(0, tracks[i] - 1),
             fxs[i], params[i], maxValues[i]
           )
         end
         paramInfo = {reaper.TrackFX_GetParam(
-          reaper.GetLastTouchedTrack(), fxs[i], params[i]
+          reaper.GetTrack(0, tracks[i] - 1), fxs[i], params[i]
         )}
         if paramInfo[1] == 0 then
           reaper.StuffMIDIMessage(
@@ -173,7 +224,6 @@ function setParamAndLed(track)
           reaper.StuffMIDIMessage(
             deviceMode, msgType + channel, ccs[i], 127
           )
-    
         end
       end
     end
@@ -195,6 +245,27 @@ function ui()
     quitBtnBounds[4],
     false
   )
+  gfx.rect(
+    resetBtnBounds[1],
+    resetBtnBounds[2],
+    resetBtnBounds[3],
+    resetBtnBounds[4],
+    false
+  )
+  --[[learnBtnBounds = {5, 5, 60, 20}
+  quitBtnBounds = {335, 5, 60, 20}
+  resetBtnBounds = {335, 30, 60, 20}]]
+  gfx.x = 10
+  gfx.y = 10
+  gfx.drawstr("Learn")
+  gfx.x = 345
+  gfx.y = 10
+  gfx.drawstr("Quit")
+  gfx.x = 345
+  gfx.y = 35
+  gfx.drawstr("Reset")
+  
+gfx.line(0, 55, 400, 55)
 
   mouseState = gfx.mouse_cap & 1 == 1
   if mouseState and
@@ -214,29 +285,45 @@ function ui()
   gfx.mouse_y < quitBtnBounds[4] + quitBtnBounds[2] then
     terminate = true
   end
+  if mouseState and
+  not lastMouseState and
+  gfx.mouse_x > resetBtnBounds[1] and
+  gfx.mouse_y > resetBtnBounds[2] and
+  gfx.mouse_x < resetBtnBounds[3] + resetBtnBounds[1] and
+  gfx.mouse_y < resetBtnBounds[4] + resetBtnBounds[2] then
+    learningState = false
+    tracks = {}
+    ccs = {}
+    fxs = {}
+    params = {}
+    minValues = {}
+    maxValues = {}
+    fxNames = {}
+ 
+    reaper.SetExtState(scriptName, "tracks", table.concat(tracks, ","), false)
+    reaper.SetExtState(scriptName, "ccs", table.concat(ccs, ","), false)
+    reaper.SetExtState(scriptName, "fxs", table.concat(fxs, ","), false)
+    reaper.SetExtState(scriptName, "params", table.concat(params, ","), false)
+    reaper.SetExtState(scriptName, "minValues", table.concat(minValues, ","), false)
+    reaper.SetExtState(scriptName, "maxValues", table.concat(maxValues, ","), false)
+    reaper.SetExtState(scriptName, "fxNames", table.concat(fxNames, ","), false)
+    
+    reaper.ClearConsole()
+  end
   lastMouseState = mouseState
-  --[[learnBtnBounds = {5, 5, 60, 20}
-  quitBtnBounds = {235, 5, 60, 20}]]
-  gfx.x = 10
-  gfx.y = 10
-  gfx.drawstr("Learn")
-  gfx.x = 345
-  gfx.y = 10
-  gfx.drawstr("Quit")
   
   gfx.x = 75
   gfx.y = 10
   gfx.drawstr(learningStateText)
-  
   gfx.x = 5
-  gfx.y = 30
+  gfx.y = 60
   gfx.drawstr(mappedCCsText)
-       
+  
 end
 
 function onExit()
+  reaper.ShowConsoleMsg("Exited")
   gfx.quit()
-  reaper.ShowConsoleMsg("\nExited\n");
 end
 
 main()
