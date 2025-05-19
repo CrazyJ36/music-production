f3fdbb8f25a6ba86ab2391d569bab290b04cc809
CrazyJ36 <crazy_j36@hotmail.com>
2025-05-19 06:10:38 -0400

    u

--- reaper-reascript/Nektar Pacer LEDs - Learn Mode.lua
+++ reaper-reascript/Nektar Pacer LEDs - Learn Mode.lua
@@ -8,19 +8,25 @@
 --msgType = inputEvent[2]:byte(1) & 0xF0
 
 reaper.ClearConsole()
-for i = 0, reaper.GetMaxMidiOutputs() do
-   midiOutName = {reaper.GetMIDIOutputName(i, "")}
-   if midiOutName[2] == "PACER" or midiOutName[2] == "PACER MIDI1" then
-     midiOutput = i + 16
-     break
-   end
-end
 scriptName = "Nektar Pacer LEDs - Learn Mode"
-maxLearnCount = 3 -- starts at one.
+maxLearnCount = 128 -- starts at one.
 learnBtnBounds = {5, 5, 60, 20}
 quitBtnBounds = {335, 5, 60, 20}
 resetBtnBounds = {335, 30, 60, 20}
+setupBtnBounds = {270, 5, 60, 20}
 learningStateText = ""
+
+if reaper.HasExtState(scriptName, "midiOutputId") then
+  midiOutputId = reaper.GetExtState(scriptName, "midiOutputId")
+else
+  midiOutputId = ""
+end
+if reaper.HasExtState(scriptName, "midiChannel") then
+  midiChannel = reaper.GetExtState(scriptName, "midiChannel")
+else
+  midiChannel = ""
+end
+
 tracks = {}
 ccs = {}
 fxs = {}
@@ -82,48 +88,52 @@ function main()
     return
   end
 
-  if learningState then
-    if #fxNames < maxLearnCount then
-      if not gotCC then
-        reaper.runloop(learnCC())
-      elseif not gotParam then
-        reaper.runloop(learnParam())  
+  if midiOutputId ~= "" and midiChannel ~= "" then
+    if learningState then
+      if #fxNames < maxLearnCount then
+        if not gotCC then
+          reaper.runloop(learnCC())
+        elseif not gotParam then
+          reaper.runloop(learnParam())  
+        end
+      else
+        learningState = false
       end
-    else
-      learningState = false
+    elseif #fxNames > 0 then
+      inputEventIn = getInputEvent()
+      for i = 1, #fxNames do
+        if tonumber(tracks[i]) == -1 then
+          trackIn = reaper.GetMasterTrack(0)
+        else
+          trackIn = reaper.GetTrack(0, tracks[i])
+        end
+        if inputEventIn[1] then
+            setParam(inputEventIn, i, trackIn)
+        end
+        setLed(i, trackIn)
+      end
     end
-  elseif #fxNames > 0 then
-    inputEventIn = getInputEvent()
+    if not learningState then
+      if #fxNames < maxLearnCount then
+        learningStateText = "Press Learn"
+      else
+        learningStateText = "Max CCs Learned"
+      end
+    end
+    mappedCCsText = ""
     for i = 1, #fxNames do
       if tonumber(tracks[i]) == -1 then
-        trackIn = reaper.GetMasterTrack(0)
-      else
-        trackIn = reaper.GetTrack(0, tracks[i])
+        trackText = "Master"
+      else 
+        trackText = tracks[i] + 1
       end
-      if inputEventIn[1] then
-          setParam(inputEventIn, i, trackIn)
-      end
-      setLed(i, trackIn)
+      mappedCCsText = mappedCCsText.."CC "..
+        ccs[i].." is mapped to track "..trackText..":\n"..fxNames[i].."\n\n"
     end
-  end
-
-  if not learningState then
-    if #fxNames < maxLearnCount then
-      learningStateText = "Press Learn"
-    else
-      learningStateText = "Max CCs Learned"
-    end
-  end
-
-  mappedCCsText = ""
-  for i = 1, #fxNames do
-    if tonumber(tracks[i]) == -1 then
-      trackText = "Master"
-    else 
-      trackText = tracks[i] + 1
-    end
-    mappedCCsText = mappedCCsText.."CC "..
-      ccs[i].." is mapped to track "..trackText..":\n"..fxNames[i].."\n\n"
+  else
+    learningStateText = "Press Setup!"
+    mappedCCsText = ""
+    learningState = false
   end
 
   ui()
@@ -215,9 +225,6 @@ function learnParam()
 end
 
 function setLed(i, trackOut) 
-  local recentInputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
-  local channel = currentInputEvent[2]:byte(1) & 0x0F
-  local msgType = currentInputEvent[2]:byte(1) & 0xF0
   paramInfo = {reaper.TrackFX_GetParam(
     trackOut, fxs[i], params[i]
   )}
@@ -227,7 +234,7 @@ function setLed(i, trackOut)
     value = 127
   end
   reaper.StuffMIDIMessage(
-    midiOutput, msgType + channel, ccs[i], value
+    midiOutputId, 176 + midiChannel, ccs[i], value
   )
 end
 
@@ -267,9 +274,18 @@ function ui()
     resetBtnBounds[4],
     false
   )
+  gfx.rect(
+    setupBtnBounds[1],
+    setupBtnBounds[2],
+    setupBtnBounds[3],
+    setupBtnBounds[4],
+    false
+  )
   --[[learnBtnBounds = {5, 5, 60, 20}
   quitBtnBounds = {335, 5, 60, 20}
-  resetBtnBounds = {335, 30, 60, 20}]]
+  resetBtnBounds = {335, 30, 60, 20}
+  outputIdBtnBounds = {265, 30, 60, 20}
+  channelBtnBounds = {265, 5, 60, 20}]]
   gfx.x = 10
   gfx.y = 10
   gfx.drawstr("Learn")
@@ -279,10 +295,13 @@ function ui()
   gfx.x = 345
   gfx.y = 35
   gfx.drawstr("Reset")
+  gfx.x = 280
+  gfx.y = 10
+  gfx.drawstr("Setup")
   
 gfx.line(0, 55, 400, 55)
 
-  mouseState = gfx.mouse_cap & 1 == 1
+  local mouseState = gfx.mouse_cap & 1 == 1
   if mouseState and
   not lastMouseState and
   gfx.mouse_x > learnBtnBounds[1] and
@@ -317,12 +336,51 @@ gfx.line(0, 55, 400, 55)
     reaper.SetExtState(scriptName, "tracks", table.concat(tracks, ","), false)
     reaper.SetExtState(scriptName, "ccs", table.concat(ccs, ","), false)
     reaper.SetExtState(scriptName, "fxs", table.concat(fxs, ","), false)
-    reaper.SetExtState(scriptName, "params", table.concat(params, ","), false)
-    reaper.SetExtState(scriptName, "minValues", table.concat(minValues, ","), false)
-    reaper.SetExtState(scriptName, "maxValues", table.concat(maxValues, ","), false)
-    reaper.SetExtState(scriptName, "fxNames", table.concat(fxNames, ","), false)
-    
+    reaper.SetExtState(scriptName, "params", table.concat(params, ","), true)
+    reaper.SetExtState(scriptName, "minValues", table.concat(minValues, ","), true)
+    reaper.SetExtState(scriptName, "maxValues", table.concat(maxValues, ","), true)
+    reaper.SetExtState(scriptName, "fxNames", table.concat(fxNames, ","), true)
+    midiOutputId = ""
+    midiChannel = ""
+    reaper.DeleteExtState(scriptName, "midiOutputId", true)
+    reaper.DeleteExtState(scriptName, "midiChannel", true)
     reaper.ClearConsole()
+  end
+  if mouseState and
+  not lastMouseState and
+  gfx.mouse_x > setupBtnBounds[1] and
+  gfx.mouse_y > setupBtnBounds[2] and
+  gfx.mouse_x < setupBtnBounds[3] + setupBtnBounds[1] and
+  gfx.mouse_y < setupBtnBounds[4] + setupBtnBounds[2] then
+  
+  setupInput = {reaper.GetUserInputs(
+    "Setup", 2, "MIDI Output Device ID,MIDI Channel of Device", 
+    midiOutputId..","..midiChannel
+  )}
+
+  if setupInput[1] then
+    string = setupInput[2]
+    reaper.ShowConsoleMsg("input string:"..string.."\n")
+    iter = 0
+    for i in string.gmatch(string, "[^,]+") do
+       if string.find(i, "") then
+         reaper.ShowConsoleMsg("i is blank at "..iter.."\n")
+       end
+       if iter == 0 then midiOutputId = i
+       else midiChannel = i
+       end
+       iter = iter + 1
+    end
+    if midiOutputId == "" then
+      reaper.ShowConsoleMsg("no output id\n")
+    end
+    if midiChannel == "" then
+      reaper.ShowConsoleMsg("no channel\n")
+    end
+    reaper.SetExtState(scriptName, "midiOutputId", midiOutputId, true)
+    reaper.SetExtState(scriptName, "midiChannel", midiChannel, true)
+  end
+  
   end
   lastMouseState = mouseState
   
