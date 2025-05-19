64bcdcc4ebb8d7f6b345fe678039d43bb2d24e69
CrazyJ36 <crazy_j36@hotmail.com>
2025-05-18 22:24:16 -0400

    u

--- reaper-reascript/Nektar Pacer LEDs.lua
+++ reaper-reascript/Nektar Pacer LEDs.lua
@@ -1,7 +1,7 @@
 --[[
   Specifically made for Nektar Pacer MIDI foot controller.
   Causes Reapers' 'learned' param changes in the DAW to reflect on Pacer LEDs if
-  its switches are set to CC and LED > Midi on.
+  its switches are set to CC toggle and LED > Midi on.
   
   To use in reaper: 
   Download or copy this script text to A new file, then find your Reaper resources directory 
@@ -22,41 +22,41 @@ for i = 0, reaper.GetMaxMidiOutputs() do
 end
 
 function run()
-
   inputEvent = {reaper.MIDI_GetRecentInputEvent(0)}
   if inputEvent[1] ~= 0 then
     channel = inputEvent[2]:byte(1) & 0x0F
     msgType = inputEvent[2]:byte(1) & 0xF0
     if msgType == 176 then
-      fx = {reaper.GetLastTouchedFX()}
+      fx = {reaper.GetTouchedOrFocusedFX(0)}
       if fx[1] then
-        for i = 1, reaper.CountTracks(0) do
-          track = reaper.GetTrack(0, i - 1)
+        for i = 0, reaper.CountTracks(0) do
+          if i == 0 then
+            track = reaper.GetMasterTrack(0)
+          else 
+            track = reaper.GetTrack(0, i - 1)
+          end
+          --reaper.ShowConsoleMsg("Checking track: "..i.."\n")
           mappedCC = {reaper.TrackFX_GetNamedConfigParm(
             track,
-            fx[3],
-            "param."..fx[4]..".learn.midi2"
+            fx[5],
+            "param."..fx[6]..".learn.midi2"
           )}
-          
           if mappedCC[1] then
             --reaper.ShowConsoleMsg("mappedCC for last Param: "..mappedCC[2].." on track: "..i.."\n")
-          
             paramInfo = {reaper.TrackFX_GetParam(
               track, 
-              fx[3], 
-              fx[4]
+              fx[5], 
+              fx[6]
             )}
             if paramInfo[1] == 0 then
               reaper.StuffMIDIMessage(midiOutput, msgType + channel, mappedCC[2], 0)
             else 
               reaper.StuffMIDIMessage(midiOutput, msgType + channel, mappedCC[2], 127)
             end
-           
           else 
             --reaper.ShowConsoleMsg("No mapped CCs for param\n")
           end
         end
-        
       else 
         --reaper.ShowConsoleMsg("No valid last touched FX parameter\n")
       end
