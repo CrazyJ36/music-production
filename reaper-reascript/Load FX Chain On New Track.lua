4b7694ac5fe01f320f56fe12daf02d05f9b27d23
CrazyJ36 <crazy_j36@hotmail.com>
2025-12-18 05:07:17 -0500

    improve load fx chain with looks and refresh button

--- reaper-reascript/Load FX Chain On New Track.lua
+++ reaper-reascript/Load FX Chain On New Track.lua
@@ -1,57 +1,83 @@
 --[[ Load, read reaper FXChains path, click to load and FX Chain. For Windows by CrazyJ36 ]]
 
-fx_chains_dir = os.getenv("APPDATA") .. "\\REAPER\\FXChains\\" -- change this if you're on Linux or Mac.
-fx_chains = {}
-gfx.y = 10
-
-line_index = 0
-function getFxChains() 
+function getFxChains()
+  fx_chains_dir = os.getenv("APPDATA") .. "\\REAPER\\FXChains\\" -- change this if you're on Linux or Mac.
+  fx_chains = {}
+  dir_line_index = 0
+  next_item = 18
   dir = io.popen('dir "' .. fx_chains_dir .. '" /b')
   for line in dir:lines() do
-    line_index = line_index + 1
-    gfx.y = gfx.y + 20
-    fx_chains[line_index] = {
-      file = line, top = gfx.y - 19, bottom = gfx.y
+    dir_line_index = dir_line_index + 1
+    fx_chains[dir_line_index] = {
+      file = line, 
+      friendly_name = line:sub(1, -10),
+      top = next_item,
+      bottom = next_item + 16
     }
+    next_item = next_item + 17
   end
   dir:close()
 end
 getFxChains()
 
-
-gfx.init("Load FX Chain", 600, 300)
-function main()
-  mouseState = gfx.mouse_cap & 1 == 1
-  if gfx.getchar() == -1 then -- kill script on window close.
+window_width = 500
+window_height = 200
+gfx.init("Load FX Chain", window_width, window_height)
+gfx.setfont(1, "Arial", 16)
+refreshButton = {0, 0, 48, 16}
+function mainLoop()
+  if gfx.getchar() == -1 then
     return
   end
   
-  if line_index > 0 then
+  if dir_line_index == 0 then
+    gfx.x = 0
+    gfx.y = 0
+    gfx.drawstr("No FX Chains")
+  else
+    gfx.x = 1
+    gfx.y = 0
+    gfx.rect(
+      refreshButton[1],
+      refreshButton[2],
+      refreshButton[3],
+      refreshButton[4],
+      false
+    )
+    gfx.drawstr("Refresh")
+    
     for i = 1, #fx_chains, 1 do
       gfx.x = 0
       gfx.y = fx_chains[i].top
-      gfx.drawstr(fx_chains[i].file)
+      gfx.drawstr(fx_chains[i].friendly_name)
+      gfx.line(0, fx_chains[i].bottom, window_width, fx_chains[i].bottom)
+    
+      mouseState = gfx.mouse_cap & 1 == 1
       if mouseState and
         not lastMouseState and
         gfx.mouse_y > fx_chains[i].top and
         gfx.mouse_y < fx_chains[i].bottom then
-        os.execute('start "" ' .. '"' .. fx_chains_dir .. fx_chains[i].file .. '"') -- change this if you're on Linux or Mac
-        return
+          os.execute('start "" ' .. -- change this if you're on Linux or Mac
+            '"' .. fx_chains_dir .. fx_chains[i].file .. '"'
+          )
+          return
+      elseif mouseState and
+        not lastMouseState and
+        gfx.mouse_x < refreshButton[3] and
+        gfx.mouse_y < refreshButton[4] then
+          getFxChains()
+          break
       end
+      
     end
     lastMouseState = mouseState
-  else 
-    gfx.x = 0
-    gfx.y = 0
-    gfx.drawstr("No FX Chains.")
   end
   
-  reaper.defer(main)
+  reaper.defer(mainLoop)
 end
-main()
+mainLoop()
 
 function atExit()
   gfx.quit()
-  return
 end
 reaper.atexit(atExit)
