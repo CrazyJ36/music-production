2e34bb5ede23cd40200ca7ece0eaec64079c5b9a
CrazyJ36 <crazy_j36@hotmail.com>
2025-12-20 06:02:31 -0500

    u

--- reaper-reascript/Load FX Chain.lua
+++ reaper-reascript/Load FX Chain.lua
@@ -1,20 +1,21 @@
 --[[ Load, read reaper FXChains path, click to load and FX Chain. For Windows by CrazyJ36 ]]
 reaper.ClearConsole()
+help_text = "Help: Click or press Keyboard Up/Down, Enter."
 
 fx_chains = {}
 fx_chains_dir = os.getenv("APPDATA") .. "\\REAPER\\FXChains\\" -- change this if you're on Linux or Mac.
 
-item_height = 16
 function getFxChains()
   local dir_line_index = 0
-  local next_item_top = 18
+  local next_item_top = 4
+  local item_height = 18
   local dir = io.popen('dir "' .. fx_chains_dir .. '" /b')
   for line in dir:lines() do
     dir_line_index = dir_line_index + 1
     next_item_top = next_item_top + item_height
     fx_chains[dir_line_index] = {
       file = line, 
-      friendly_name = "- " .. line:sub(1, -10),
+      friendly_name = line:sub(1, -10),
       top = next_item_top,
       bottom = next_item_top + item_height
     }
@@ -23,17 +24,40 @@ function getFxChains()
 end
 getFxChains()
 
-function startFxLoad(chain)
-  os.execute('start "" ' .. -- change this if you're on Linux or Mac
-    '"' .. fx_chains_dir .. fx_chains[chain].file .. '"'
-  )
+function startFxLoad(selected_fx)
+  local fx_block_start = "<FXCHAIN\n"
+  local fx_block_end = ">\n"
+  local pattern = fx_block_start .. "(.+)" .. fx_block_end
+
+  local track = reaper.GetSelectedTrack2(0, 0, true)
+  if track == nil then
+    reaper.ShowConsoleMsg("No Track, making one.\n")
+    reaper.InsertTrackInProject(0, reaper.GetNumTracks(), 0)
+    track = reaper.GetTrack(0, reaper.GetNumTracks() - 1)
+    reaper.SetMediaTrackInfo_Value(track, "B_AUTO_RECARM", 1)
+    reaper.SetOnlyTrackSelected(track)
+  else
+    reaper.ShowConsoleMsg("Already has  Track.\n")
+  end
+  
+  local file = fx_chains_dir .. fx_chains[selected_fx].file
+  local file_loader = io.open(file, 'r')
+  local file_text = file_loader:read('*a')
+  file_loader:close()
+  
+  local retval, chunk = reaper.GetTrackStateChunk(track, '', false)
+  local new_chunk = chunk:gsub(pattern, fx_block_start .. file_text .. fx_block_end)
+  reaper.SetTrackStateChunk(track, new_chunk, true)
+  
+  --eaper.ShowConsoleMsg(new_chunk)
+  
 end
 
 window_width = 500
 window_height = 200
 gfx.init("Load FX Chain", window_width, window_height)
 gfx.setfont(1, "Arial", 16)
-refreshButton = {0, 0, 52, 16, 50}
+refresh_button = {0, 0, 53, 18, 15}
 total_sleep_time = os.clock() + 4
 selected_fx = 1
 function mainLoop()
@@ -54,18 +78,21 @@ function mainLoop()
   else
     gfx.y = 0
     gfx.x = 0
-    gfx.drawstr("Help: Click or press Keyboard Up/Down, Enter.", 2, gfx.w, gfx.h)
+    gfx.drawstr(help_text, 2, gfx.w, gfx.h)
+    gfx.roundrect(
+      refresh_button[1],
+      refresh_button[2],
+      refresh_button[3],
+      refresh_button[4],
+      refresh_button[5]
+    )
+    
     gfx.x = 5
     gfx.y = 0
-    gfx.roundrect(
-      refreshButton[1],
-      refreshButton[2],
-      refreshButton[3],
-      refreshButton[4],
-      refreshButton[5]
-    )
-    gfx.drawstr("Refresh")
-    
+    gfx.drawstr("Refresh", refresh_button[3], refresh_button[2])
+    gfx.line(0, fx_chains[selected_fx].bottom, gfx.measurestr(fx_chains[selected_fx].friendly_name), fx_chains[selected_fx].bottom) -- bottom
+    gfx.line(0, fx_chains[selected_fx].top,  0, fx_chains[selected_fx].bottom) -- side
+
     if char == 1685026670 and fx_chains[selected_fx + 1] ~= nil then -- down
       selected_fx = selected_fx + 1
     elseif char == 30064 and fx_chains[selected_fx - 1] ~= nil then -- up
@@ -74,29 +101,25 @@ function mainLoop()
       startFxLoad(selected_fx)
       return
     end
-    
     for i = 1, #fx_chains, 1 do
       gfx.x = 1
       gfx.y = fx_chains[i].top
       gfx.drawstr(fx_chains[i].friendly_name)
       
       if gfx.mouse_cap & 1 == 1 and 
+        gfx.mouse_x < gfx.measurestr(fx_chains[i].friendly_name) and
         gfx.mouse_y > fx_chains[i].top and
         gfx.mouse_y < fx_chains[i].bottom then
           startFxLoad(i) 
           return
       elseif gfx.mouse_cap & 1 == 1 and 
-        gfx.mouse_x < refreshButton[3] and
-        gfx.mouse_y < refreshButton[4] then
+        gfx.mouse_x < refresh_button[3] and
+        gfx.mouse_y < refresh_button[4] then
           getFxChains()
           break
       end
     end
-    
-    gfx.line(0, fx_chains[selected_fx].bottom, gfx.measurestr(fx_chains[selected_fx].friendly_name), fx_chains[selected_fx].bottom) -- bottom
-    gfx.line(0, fx_chains[selected_fx].top,  0, fx_chains[selected_fx].bottom) -- side
-    
-  
+
   end
   
   reaper.defer(mainLoop)
@@ -105,6 +128,5 @@ mainLoop()
 
 function atExit()
   gfx.quit()
-  reaper.ShowConsoleMsg("Killed\n")
 end
 reaper.atexit(atExit)
